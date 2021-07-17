94d708903528f35e9028532a7c3ce2d9e3afb1ed
AR <techrebel2723@gmail.com>
2021-07-17 23:07:26 +0000

    .

--- frontend/components/loading.tsx
+++ frontend/components/loading.tsx
@@ -1,5 +1,7 @@
-
-
+//import { LinearProgress } from "@material-ui/core";
+var tips = ["Hangle is open source on GitHub, HangleHQ/next", "You can use markdown in your messages", "We have a Discord, hangle.me/discord"]
+import Progress from "@material-ui/core/CircularProgress";
+// ignore that center error, it works anyway
 export default function Loading() {
     return (
         <>
@@ -9,16 +11,16 @@ export default function Loading() {
                     <br />
                     <br />
                     <br />
-                    <img alt="hangle loading..." src={window.CONFIG.LOADING_IMAGE} />
-                    <h1>Connecting to Hangle...</h1>
-                    <p>
-                        We're loading your conversations, please wait. Having problems?
-                        Take a look at the{" "}
-                        <a href="https://hangle.me/docs/troubleshooting/connecting">
-                        troubleshooting guide
-                </a>
-                .
+                    <center> 
+                    <Progress />
+                    <div className="card p-3 rounded-md m-4 text-blue bgs">
+                    <h1 className="bgs">Did you know</h1>
+                    <p className="bgs">
+                       {tips[Math.floor(Math.random() * tips.length)]}
+
               </p>
+              </div>
+              </center>
                 </div>
             </div>
         </>
